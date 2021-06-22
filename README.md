13fde0e649f828a81b31ed0d53a6f9b4edee5879
Nadir Lloret <nadir.lloret@gmail.com>
2021-06-22 22:30:08 +0100

    feat: initialize repo

---
+++ README.md
@@ -0,0 +1,133 @@
+# vault-init
+
+This is a port of [Kelsey Hightower](https://github.com/kelseyhightower) [vault-init](https://github.com/kelseyhightower/vault-init) to AWS.
+
+The `vault-init` service automates the process of [initializing](https://www.vaultproject.io/docs/commands/operator/init.html) and [unsealing](https://www.vaultproject.io/docs/concepts/seal.html#unsealing) HashiCorp Vault instances running on [Amazon Web Services](http://aws.amazon.com/).
+
+After `vault-init` initializes a Vault server it stores master keys and root tokens, encrypted using [AWS Key Management Service](https://aws.amazon.com/kms/), to a user defined [Amazon S3](https://aws.amazon.com/s3/) bucket.
+
+## Usage
+
+The `vault-init` service is designed to be run alongside a Vault server and communicate over local host.
+
+## Configuration
+
+The vault-init service supports the following environment variables for configuration:
+
+* `CHECK_INTERVAL` - The time in seconds between Vault health checks. (300)
+* `S3_BUCKET_NAME` - The Amazon S3 Bucket where the vault master key and root token is stored.
+* `KMS_KEY_ID` - The Amazon KMS key ID used to encrypt and decrypt the vault master key and root token.
+* `VAULT_ADDR` - The vault API address.
+
+### Example Values
+
+```
+CHECK_INTERVAL="300"
+S3_BUCKET_NAME="vault-storage"
+KMS_KEY_ID="arn:aws:kms:us-east-1:1234567819:key/dead-beef-dead-beef-deadbeefdead"
+VAULT_ADDR="https://vault.service.consul:8200"
+```
+
+### AWS
+
+The `vault-init` service needs the following set of resources:
+
+- S3 Bucket
+- IAM Role + Instance Profile
+- KMS Key
+
+Here's a minimal example which creates an instance profile that can use a KMS key and read/write to a private S3 bucket.
+
+```hcl
+resource "aws_iam_role" "vault" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {"Service": "ec2.amazonaws.com"},
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# use the current caller's ARN as the KMS key administrator
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "vault" {
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "vault-key-policy",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {"AWS": "${data.aws_caller_identity.current.arn}"},
+      "Action": "kms:*",
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": {"AWS": "${aws_iam_role.vault.arn}"},
+      "Action": [
+	"kms:Encrypt",
+	"kms:Decrypt",
+	"kms:ReEncrypt*",
+	"kms:GenerateDataKey*",
+	"kms:DescribeKey"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_kms_alias" "vault" {
+  name          = "alias/my-vault-key"
+  target_key_id = "${aws_kms_key.vault.key_id}"
+}
+
+resource "aws_s3_bucket" "vault" {
+  acl = "private"
+}
+
+resource "aws_iam_role_policy" "vault" {
+  role	 = "${aws_iam_role.vault.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+	"kms:ReEncrypt*",
+	"kms:GenerateDataKey*",
+	"kms:Encrypt",
+	"kms:DescribeKey",
+	"kms:Decrypt"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_kms_alias.vault.arn}"
+    },
+    {
+      "Action": "s3:ListBucket",
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.vault.arn}"
+    },
+    {
+      "Action": ["s3:GetObject", "s3:PutObject"],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.vault.arn}/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "vault" {
+  role = "${aws_iam_role.vault.name}"
+}
+```
---
+++ main.go
@@ -0,0 +1,339 @@
+// Copyright 2018 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var (
+	vaultAddr     string
+	checkInterval string
+	s3BucketName  string
+	httpClient    http.Client
+	kmsKeyId      string
+)
+
+// InitRequest holds a Vault init request.
+type InitRequest struct {
+	SecretShares    int `json:"secret_shares"`
+	SecretThreshold int `json:"secret_threshold"`
+}
+
+// InitResponse holds a Vault init response.
+type InitResponse struct {
+	Keys       []string `json:"keys"`
+	KeysBase64 []string `json:"keys_base64"`
+	RootToken  string   `json:"root_token"`
+}
+
+// UnsealRequest holds a Vault unseal request.
+type UnsealRequest struct {
+	Key   string `json:"key"`
+	Reset bool   `json:"reset"`
+}
+
+// UnsealResponse holds a Vault unseal response.
+type UnsealResponse struct {
+	Sealed   bool `json:"sealed"`
+	T        int  `json:"t"`
+	N        int  `json:"n"`
+	Progress int  `json:"progress"`
+}
+
+func main() {
+	log.Println("Starting the vault-init service...")
+
+	vaultAddr = os.Getenv("VAULT_ADDR")
+	if vaultAddr == "" {
+		vaultAddr = "https://127.0.0.1:8200"
+	}
+
+	checkInterval = os.Getenv("CHECK_INTERVAL")
+	if checkInterval == "" {
+		checkInterval = "10"
+	}
+
+	i, err := strconv.Atoi(checkInterval)
+	if err != nil {
+		log.Fatalf("CHECK_INTERVAL is invalid: %s", err)
+	}
+
+	checkIntervalDuration := time.Duration(i) * time.Second
+
+	s3BucketName = os.Getenv("S3_BUCKET_NAME")
+	if s3BucketName == "" {
+		log.Fatal("S3_BUCKET_NAME must be set and not empty")
+	}
+
+	kmsKeyId = os.Getenv("KMS_KEY_ID")
+	if kmsKeyId == "" {
+		log.Fatal("KMS_KEY_ID must be set and not empty")
+	}
+	
+	timeout := 2 * time.Second
+	
+	httpClient = http.Client{
+		Timeout: timeout,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+
+	for {
+		response, err := httpClient.Get(vaultAddr + "/v1/sys/health")
+		if response != nil && response.Body != nil {
+			_ = response.Body.Close()
+		}
+
+		if err != nil {
+			log.Println(err)
+			time.Sleep(checkIntervalDuration)
+			continue
+		}
+
+		switch response.StatusCode {
+		case 200:
+			log.Println("Vault is initialized and unsealed.")
+		case 429:
+			log.Println("Vault is unsealed and in standby mode.")
+		case 501:
+			log.Println("Vault is not initialized. Initializing and unsealing...")
+			initialize()
+			unseal()
+		case 503:
+			log.Println("Vault is sealed. Unsealing...")
+			unseal()
+		default:
+			log.Printf("Vault is in an unknown state. Status code: %d", response.StatusCode)
+		}
+
+		log.Printf("Next check in %s", checkIntervalDuration)
+		time.Sleep(checkIntervalDuration)
+	}
+}
+
+func initialize() {
+	initRequest := InitRequest{
+		SecretShares:    5,
+		SecretThreshold: 3,
+	}
+
+	initRequestData, err := json.Marshal(&initRequest)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	r := bytes.NewReader(initRequestData)
+
+	request, err := http.NewRequest("PUT", vaultAddr+"/v1/sys/init", r)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	response, err := httpClient.Do(request)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	initRequestResponseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if response.StatusCode != 200 {
+		log.Printf("init: non 200 status code: %d", response.StatusCode)
+		return
+	}
+
+	var initResponse InitResponse
+
+	if err := json.Unmarshal(initRequestResponseBody, &initResponse); err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("Encrypting unseal keys and the root token and uploading to bucket...")
+
+	AWSSession, err := session.NewSession()
+	if err != nil {
+		log.Println("Error creating session: ", err)
+	}
+
+	KMSService := kms.New(AWSSession)
+	S3Service := s3.New(AWSSession)
+
+	// Encrypt root token.
+	rootTokenEncryptedData, err := KMSService.Encrypt(&kms.EncryptInput{
+		KeyId:     aws.String(kmsKeyId),
+		Plaintext: []byte(initResponse.RootToken),
+	})
+	if err != nil {
+		log.Println("Error encrypting root token: ", err)
+	}
+
+	// Encrypt unseal keys.
+	unsealKeysEncryptedData, err := KMSService.Encrypt(&kms.EncryptInput{
+		KeyId:     aws.String(kmsKeyId),
+		Plaintext: []byte(base64.StdEncoding.EncodeToString(initRequestResponseBody)),
+	})
+	if err != nil {
+		log.Println("Error encrypting unseal keys: ", err)
+	}
+
+	// Save the encrypted root token.
+	rootTokenPutRequest := &s3.PutObjectInput{
+		Body:   bytes.NewReader(rootTokenEncryptedData.CiphertextBlob),
+		Bucket: aws.String(s3BucketName),
+		Key:    aws.String("root-token.json.enc"),
+	}
+
+	_, err = S3Service.PutObject(rootTokenPutRequest)
+	if err != nil {
+		log.Printf("Cannot write root token to bucket s3://%s/%s: %s", s3BucketName, "root-token.json.enc", err)
+	} else {
+		log.Printf("Root token written to s3://%s/%s", s3BucketName, "root-token.json.enc")
+	}
+
+	// Save the encrypted unseal keys.
+	unsealKeysEncryptRequest := &s3.PutObjectInput{
+		Body:   bytes.NewReader(unsealKeysEncryptedData.CiphertextBlob),
+		Bucket: aws.String(s3BucketName),
+		Key:    aws.String("unseal-keys.json.enc"),
+	}
+
+	_, err = S3Service.PutObject(unsealKeysEncryptRequest)
+	if err != nil {
+		log.Printf("Cannot write unseal keys to bucket s3://%s/%s: %s", s3BucketName, "unseal-keys.json.enc", err)
+	} else {
+		log.Printf("Unseal keys written to s3://%s/%s", s3BucketName, "unseal-keys.json.enc")
+	}
+
+	log.Println("Initialization complete.")
+}
+
+func unseal() {
+
+	AWSSession, err := session.NewSession()
+	if err != nil {
+		log.Println("Error creating session: ", err)
+	}
+
+	KMSService := kms.New(AWSSession)
+	S3Service := s3.New(AWSSession)
+
+	unsealKeysRequest := &s3.GetObjectInput{
+		Bucket: aws.String(s3BucketName),
+		Key:    aws.String("unseal-keys.json.enc"),
+	}
+
+	unsealKeysEncryptedObject, err := S3Service.GetObject(unsealKeysRequest)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	unsealKeysEncryptedObjectData, err := ioutil.ReadAll(unsealKeysEncryptedObject.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	unsealKeysData, err := KMSService.Decrypt(&kms.DecryptInput{
+		CiphertextBlob: unsealKeysEncryptedObjectData,
+	})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var initResponse InitResponse
+
+	unsealKeysPlaintext, err := base64.StdEncoding.DecodeString(string(unsealKeysData.Plaintext))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if err := json.Unmarshal(unsealKeysPlaintext, &initResponse); err != nil {
+		log.Println(err)
+		return
+	}
+
+	for _, key := range initResponse.KeysBase64 {
+		done, err := unsealOne(key)
+		if done {
+			return
+		}
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func unsealOne(key string) (bool, error) {
+	unsealRequest := UnsealRequest{
+		Key: key,
+	}
+
+	unsealRequestData, err := json.Marshal(&unsealRequest)
+	if err != nil {
+		return false, err
+	}
+
+	r := bytes.NewReader(unsealRequestData)
+	request, err := http.NewRequest(http.MethodPut, vaultAddr+"/v1/sys/unseal", r)
+	if err != nil {
+		return false, err
+	}
+
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return false, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return false, fmt.Errorf("unseal: non-200 status code: %d", response.StatusCode)
+	}
+
+	unsealRequestResponseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return false, err
+	}
+
+	var unsealResponse UnsealResponse
+	if err := json.Unmarshal(unsealRequestResponseBody, &unsealResponse); err != nil {
+		return false, err
+	}
+
+	if !unsealResponse.Sealed {
+		return true, nil
+	}
+
+	return false, nil
+}
