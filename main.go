e134363dab14541547fa1afd61fa3310f3e57301
Nadir Lloret <nadir.lloret@gmail.com>
2022-05-04 23:33:26 +0100

    feat(seal): do not implement unseal process. Use Vault built in cloud mechanism

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"crypto/tls"
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"github.com/aws/aws-sdk-go/service/kms"
 	"io/ioutil"
 	"log"
@@ -63,7 +62,7 @@ type UnsealResponse struct {
 }
 
 func main() {
-	log.Println("Starting the vault-init service...")
+	log.Println("Starting vault-init service...")
 
 	vaultAddr = os.Getenv("VAULT_INIT_VAULT_ADDR")
 	if vaultAddr == "" {
@@ -90,9 +89,9 @@ func main() {
 	ssmKeysPath = "/VAULT/" + ssmSuffix + "/UNSEAL_KEYS"
 	ssmTokenPath = "/VAULT/" + ssmSuffix + "/ROOT_TOKEN"
 
-	kmsKeyId = os.Getenv("VAULT_INIT_KMS_KEY_ID")
+	kmsKeyId = os.Getenv("VAULT_KMS_KEY_ID")
 	if kmsKeyId == "" {
-		log.Fatal("VAULT_INIT_KMS_KEY_ID must be set and not empty")
+		log.Fatal("VAULT_KMS_KEY_ID must be set and not empty")
 	}
 
 	timeout := 2 * time.Second
@@ -125,12 +124,12 @@ func main() {
 		case 429:
 			log.Println("Vault is unsealed and in standby mode. Nothing to do")
 			os.Exit(0)
+		case 503:
+			log.Println("Vault is initialized and sealed. Nothing to do")
+			os.Exit(0)
 		case 501:
 			log.Println("Vault is not initialized. Initializing...")
 			initialize()
-			os.Exit(0)
-		case 503:
-			log.Println("Vault is initialized and sealed. Nothing to do")
 			os.Exit(0)
 		default:
 			log.Printf("Vault is in an unknown state. Status code: %d", response.StatusCode)
@@ -207,7 +206,7 @@ func initialize() {
 
 	SSMService := ssm.New(AWSSession)
 
-	// Save the encrypted root token.
+	// Save the root token.
 	rootTokenPutRequest := &ssm.PutParameterInput {
 		Name: aws.String(ssmTokenPath),
 		Type: aws.String("SecureString"),
@@ -223,7 +222,7 @@ func initialize() {
 		log.Printf("Root token written to ssm://%s", ssmTokenPath)
 	}
 
-	// Save the encrypted unseal keys.
+	// Save the unseal keys.
 	unsealKeysEncryptRequest := &ssm.PutParameterInput {
 		Name: aws.String(ssmKeysPath),
 		Type: aws.String("SecureString"),
@@ -240,92 +239,4 @@ func initialize() {
 	}
 
 	log.Println("Initialization complete.")
-}
-
-func unseal() {
-
-	AWSSession, err := session.NewSession()
-	if err != nil {
-		log.Println("Error creating session: ", err)
-	}
-
-	SSMService := ssm.New(AWSSession)
-
-	unsealKeysRequest := &ssm.GetParameterInput{
-		Name: aws.String(ssmKeysPath),
-		WithDecryption: aws.Bool(true),
-	}
-
-	unsealKeysData, err := SSMService.GetParameter(unsealKeysRequest)
-	if err != nil {
-		log.Printf("Cannot read unseal keys from ssm at ssm://%s: %s", ssmKeysPath, err)
-	}
-
-	var initResponse InitResponse
-
-	unsealKeysPlaintext, err := base64.StdEncoding.DecodeString(*unsealKeysData.Parameter.Value)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	if err := json.Unmarshal(unsealKeysPlaintext, &initResponse); err != nil {
-		log.Println(err)
-		return
-	}
-
-	for _, key := range initResponse.KeysBase64 {
-		done, err := unsealOne(key)
-		if done {
-			return
-		}
-
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
-func unsealOne(key string) (bool, error) {
-	unsealRequest := UnsealRequest{
-		Key: key,
-	}
-
-	unsealRequestData, err := json.Marshal(&unsealRequest)
-	if err != nil {
-		return false, err
-	}
-
-	r := bytes.NewReader(unsealRequestData)
-	request, err := http.NewRequest(http.MethodPut, vaultAddr+"/v1/sys/unseal", r)
-	if err != nil {
-		return false, err
-	}
-
-	response, err := httpClient.Do(request)
-	if err != nil {
-		return false, err
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != 200 {
-		return false, fmt.Errorf("unseal: non-200 status code: %d", response.StatusCode)
-	}
-
-	unsealRequestResponseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return false, err
-	}
-
-	var unsealResponse UnsealResponse
-	if err := json.Unmarshal(unsealRequestResponseBody, &unsealResponse); err != nil {
-		return false, err
-	}
-
-	if !unsealResponse.Sealed {
-		return true, nil
-	}
-
-	return false, nil
 }
